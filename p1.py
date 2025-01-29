4741e3174eb2187d61f4633f091658b136b19e8f
sHeughan <147839762+sHeughan@users.noreply.github.com>
2025-01-29 19:34:01 +0000

    p1 + p2 done

--- p1.py
+++ p1.py
@@ -1,2 +1,33 @@
+Letters = {"A": 65, "B": 66, "C": 67, "D": 68, "E": 69, "F": 70, "G": 71, "H": 72, 
+            "I": 73, "J": 74, "K": 75, "L": 76, "M": 77, "N": 78, "O": 79, "P": 80,
+            "Q": 81, "R": 82, "S": 83, "T": 84, "U": 85, "V": 86, "W": 87, "X": 88,
+            "Y": 89, "Z": 90, "a": 97, "b": 98, "c": 99, "d": 100,"e": 101,"f": 102,
+            "g": 103,"h": 104,"i": 105,"j": 106,"k": 107,"l": 108,"m": 109,"n": 110,
+            "o": 111,"p": 112,"q": 113,"r": 114,"s": 115,"t": 116,"u": 117,"v": 118,
+            "w": 119,"x": 120,"y": 121,"z": 122
+            }
+
+def hash(s):
+    hash_num = 0
+    for x in s:
+        hash_num += Letters[x]
+    hash_num = hash_num % 100
+    #print(hash_num)
+    return hash_num
+
 def group_anagrams(words):
-    pass
+    anagrams = {}
+    for x in words:
+        if hash(x) not in anagrams:
+            anagrams[hash(x)] = [x]
+        
+        else:
+            anagrams[hash(x)].append(x)
+
+    final_list = []
+    for x in anagrams.values():
+        final_list += [x]
+    return final_list
+
+words = ["bat", "meta", "tab", "tea", "ate", "tame", "jazz", "eat"]
+print(group_anagrams(words))
--- p2.py
+++ p2.py
@@ -1,2 +1,40 @@
 def k_most_frequent(lst, k):
-    pass
+    
+    elements = {}
+
+    for x in lst:
+        if x not in elements:
+            elements[x] = 1
+        
+        else:
+            elements.update({x: elements[x] + 1})
+
+    print(elements)
+
+    items = []
+    keys = []
+    for x, y in elements.items():
+        items.append((x, y))
+        keys.append(x)
+
+    if k >= len(keys): return keys
+
+    items.sort(key=lambda x: x[1], reverse = True)
+
+    print(items)
+
+    final_elements = []
+    for x in range(len(items)):
+        print((items[x])[0])
+        print(k)
+        print("___")
+        final_elements.append((items[x])[0])
+        if x != len(items) - 1:
+            if (items[x])[1] != (items[x+1])[1]: k -= 1
+        
+        if k == 0: break
+
+    return final_elements
+
+
+print(k_most_frequent([1, 1, 1, 2, 2, 3], 2))
